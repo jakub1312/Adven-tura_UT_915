6ea4081a3282aece64f23a4650e214159278a8d9
jakub1312 <ismj00@vse.cz>
2017-10-17 10:48:00 +0200

    add image

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
